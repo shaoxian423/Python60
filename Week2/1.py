e18e144e3da9fc0f8e83ec87ac6f5a7c2308f7d0
Shaoxian Duan <duan0027@algonquinlive.com>
2025-09-09 17:25:28 -0400

    add patten13

--- Week2/1.py
+++ Week2/1.py
@@ -10,8 +10,6 @@ if os.path.exists(FILE_NAME):
 else:
     books = []
 
-    for ()
-
 
 def save_books():
     with open(FILE_NAME, "w") as f:
@@ -43,6 +41,7 @@ def query_books():
         print("没有书籍信息")
         return
     print("当前书籍列表:")
+    # enumberate是枚举器的意思,就是在遍历可迭代对象的时候,同时拿到元素的索引和值
     for i, book in enumerate(books, start=1):
         print(f"{i}. {book['title']} - {book['author']} ({book['year']})")
 
@@ -51,7 +50,7 @@ def query_books():
 while True:
     print("\n1-添加书籍  2-删除书籍  3-查看书籍  4-退出")
     choice = input("请选择操作: ")
-    if choice == "1":
+    if choice == "1":  # 必须用“”把数字括起来,因为用户输入的是数字,必须把它变成字符串再来比较,或用.strip()
         add_book()
     elif choice == "2":
         delete_book()
